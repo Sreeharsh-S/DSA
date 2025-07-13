b0b2442d76f48093a28d924220e37ba3d93d4070
Sreehari78 <sreehari1672000@gmail.com>
2025-07-13 17:55:29 +0530

    Add pattern generation programs and .gitignore file

---
+++ Pattern Problems/Pattern1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void nForest(int n) {
+	// Write your code here.
+int i,j;
+    for(i = 0;i < n; i++){
+
+        for(j = 0;j <n; j++){
+            cout << "*";
+        }
+    cout <<"\n";
+    }
+}
+int main(){
+
+    int n;
+    cout << "Enter the Number of rows and columns";
+    cin >> n;
+
+    nForest(n);
+    return 0;
+}
---
+++ Pattern Problems/Pattern6.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void nForest(int n) {
+	// Write your code here.
+int i,j;
+    for(i = 0;i < n; i++){
+
+        for(j = 1;j <n-i+1; j++){
+            cout << j;
+        }
+    cout <<"\n";
+    }
+}
+int main(){
+
+    int n;
+    cout << "Enter the Number of rows and columns";
+    cin >> n;
+
+    nForest(n);
+    return 0;
+}
