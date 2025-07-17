97f6e41ac3e914cc9ab1996f44d1719e8b9006f5
Sreeharsh-S <sreeharsh7x@gmail.com>
2025-07-18 00:28:50 +0530

    Fix loop condition in nForest function to prevent extra row output

--- Pattern Problems/Pattern13.cpp
+++ Pattern Problems/Pattern13.cpp
@@ -6,7 +6,7 @@ void nForest(int n) {
 	// Write your code here.
 int i,j;
 int c=1;
-    for(i = 0;i <= n; i++){
+    for(i = 0;i < n; i++){
 
         for(j = 0;j <i; j++){
             cout << c << " ";
